68ee0baed70a200f58aea1872a7030025fcb39c5
Paul Thanataweenont <p.t.thanataweenont@gmail.com>
2022-05-14 20:08:02 -0600

    IntroBox component initial release

---
+++ renderer/pages/page1.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import IntroBox from "../components/IntroBox/IntroBox.jsx"
+
+function pageOne () {
+  return(
+    <div>
+      <IntroBox 
+        dataIndex={0}
+      />
+    </div>
+  )
+}
+export default pageOne
