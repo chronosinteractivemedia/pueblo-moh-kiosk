dc1b867379c85788dd72b41b6c3b8130695544e6
Jordan Sullivan <jmsull@protonmail.com>
2022-05-20 10:47:14 -0600

    some ideas on structure

--- renderer/pages/1-pueblo-recepients.jsx
+++ renderer/pages/1-pueblo-recepients.jsx
@@ -1,16 +1,36 @@
 import React, {useState, useEffect} from "react"
 import SectionLeft from "../components/SectionLeft/SectionLeft.jsx"
+import GalleryPerson from "../components/GalleryPerson/GalleryPerson";
+import IntroBox from "../components/IntroBox/IntroBox";
 
 function pageOne () {
-  const [leftCategory, setLeftCategory] = useState('intro')
-  // parse the entire object later.
+  const [currentRecipient, setCurrentRecipient] = useState(null);
   return(
     <div className="page-flex-row">
-      <SectionLeft 
-        category = {leftCategory}
-      />
+      <SectionLeft width={!!currentRecipient ? "wide" : "narrow"}> //children of this are always 100%
+        {!!currentRecipient ?
+          <GalleryPerson person={currentRecipient} />
+          :
+          <IntroBox />
+        }
+      </SectionLeft>
+      <SectionRight width={!!currentRecipient ? "narrow" : "wide"}> //children of this are always 100% width
+        {!!currentRecipient ?
+          <GalleryPersonRightDetails person={currentRecipient} />
+          :
+          <GalleryPersonSelection setCurrentRecepient={setCurrentRecipient} />
+          //inside the above component, when a thumbnail is clicked, the entire json object for that person is passed to setCurrentRecipient
+        }
+      </SectionRight>
     </div>
   )
 }
 
 export default pageOne
+
+
+//to get object keys
+for(let key in jsonOnject){
+  console.log(key); //print out each key in object
+}
+
