7c9325ba6d2c0a54b06a90f8302ace746204b4a4
Jordan Sullivan <jmsull@protonmail.com>
2022-05-24 15:00:16 -0600

    Adding a bunch of functionality

---
+++ renderer/components/Modal/Modal.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDom from "react-dom";
+import styles from "./Modal.module.scss";
+import {MdClose} from "react-icons/md";
+
+export function Modal({ children, index = 0, transparent = false, onClose}) {
+  const output = (
+    <div className={styles.component} data-transparent={transparent}>
+      <div className={styles.wrapper}>
+        <div className={styles.close} onClick={onClose}><MdClose /></div>
+        {children}
+      </div>
+    </div>
+  );
+
+  let container;
+  if(typeof window !== 'undefined'){
+    container = window?.document?.querySelector(index === 0 ? "#modal0" : "#modal1");
+  }
+  return container ? ReactDom.createPortal(output, container) : null;
+}
