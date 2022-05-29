f00498bcccbaf79a52fa86fec79d92d9e28d9433
Paul Thanataweenont <p.t.thanataweenont@gmail.com>
2022-05-28 19:20:57 -0600

    update dummy data for popup

---
+++ renderer/components/Attractions/AttractionPopup.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import style from "../Attractions/Attractions.module.scss";
+import Image from "../Image/Image";
+import ImageSlider from "../ImageSlider/ImageSlider";
+import DetailPopup from "../DetailPopup/DetailPopup";
+import database from "../../../database.json"
+
+function AttractionPopup ({attractionId}){
+  return (
+    <div>
+      <DetailPopup>
+        
+      </DetailPopup>
+    </div>
+  )
+}
--- renderer/pages/area-attractions.jsx
+++ renderer/pages/area-attractions.jsx
@@ -3,16 +3,19 @@ import Header from "../components/Attractions/Header"
 import SearchBar from "../components/Attractions/SearchBar"
 import Features from "../components/Attractions/Features"
 import List from "../components/Attractions/List"
+import DetailPopup from "../components/DetailPopup/DetailPopup"
 
 function AreaAttractions () {
   return(
     <div>
-      <Header />
-      <SearchBar />
-      <div className="page-flex-row" style={{backgroundColor:"#1E2934", position:"absolute"}}>
-        <Features /> 
-        <List />
-      </div>
+      <div>
+        <Header />
+        <SearchBar />
+        <div className="page-flex-row" style={{backgroundColor:"#1E2934", position:"absolute"}}>
+          <Features /> 
+          <List />
+        </div>
+      </div>  
     </div>
   )
 }
