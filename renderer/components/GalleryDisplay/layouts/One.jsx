1f9eacba13131340777297a39fe3271563112484
Paul Thanataweenont <p.t.thanataweenont@gmail.com>
2022-06-04 20:08:23 -0600

    thumbnail_caption update

--- renderer/components/GalleryDisplay/layouts/One.jsx
+++ renderer/components/GalleryDisplay/layouts/One.jsx
@@ -8,21 +8,24 @@ import {Modal} from "../../Modal/Modal";
 function DetailOne(medias, setShowSlides){
   return (
     <div className={style.wrapper_column_two}>
-      <div className={style.media_one} onClick={() => setShowSlides(medias[0].slides)}>
-        <div className={style.gallery_control_background}>
-          <div className={style.gallery_control_wrapper}>
-            <div className={style.gallery_control_title}>{medias[0].title}</div>
-            <h3>{medias[0].type}</h3>
-            {medias[0].type.toLowerCase() === "video"? <a className={style.gallery_control_video}></a> : ''}
-            {medias[0].type.toLowerCase() === "gallery"? <a className={style.gallery_control_gallery}>Enter Gallery</a> : ''}
+      <div className={style.media_wrapper}>
+        <div className={style.media_one} onClick={() => setShowSlides(medias[0].slides)}>
+          <div className={style.gallery_control_background}>
+            <div className={style.gallery_control_wrapper}>
+              <div className={style.gallery_control_title}>{medias[0].title}</div>
+              <h3>{medias[0].type}</h3>
+              {medias[0].type.toLowerCase() === "video"? <a className={style.gallery_control_video}></a> : ''}
+              {medias[0].type.toLowerCase() === "gallery"? <a className={style.gallery_control_gallery}>Enter Gallery</a> : ''}
+            </div>
           </div>
+          <Image 
+            src={medias[0].thumbnail}
+            width={932}
+            height={621}
+            position="relative"
+          />
         </div>
-        <Image 
-          src={medias[0].thumbnail}
-          width={932}
-          height={621}
-          position="relative"
-        />
+        <div className={style.media_thumbnail_caption}>{medias[0].thumbnail_caption}</div>
       </div>
     </div>
     ) 
