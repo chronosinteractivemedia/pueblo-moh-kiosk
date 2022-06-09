0388cc24b0a7764cdb310b61e7e130765766ce5c
Jordan Sullivan <jmsull@protonmail.com>
2022-06-09 14:49:42 -0600

    more qa fixes

---
+++ renderer/components/Keyboard/Keyboard.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useRef, useEffect } from 'react';
+import Keyboard from 'react-simple-keyboard';
+import 'react-simple-keyboard/build/css/index.css';
+import styles from './Keyboard.module.scss';
+import {BsFillXCircleFill, BsSearch} from 'react-icons/bs';
+
+export default function ({ externValue = "", onChange, onCancel}) {
+    const keyboard = useRef();
+    const [layout, setLayout] = useState('default');
+    const [value, setValue] = useState(externValue);
+    const [isShift, setIsShift] = useState(false);
+    const prevExtern = useRef(externValue); 
+
+    useEffect(() => {
+        onChange(value)
+    },[value]);
+
+    useEffect(() => {
+        console.log('extern: ',externValue)
+        if(externValue !== prevExtern.current){
+            setValue(externValue);
+            prevExtern.current = externValue;
+        }
+    },[externValue, prevExtern]);
+
+
+    const handleShift = (button) => {
+        let newLayoutName = layout === "default" ? "shift" : "default";
+        if(button === "{shift}"){
+            setIsShift(true);
+        } else {
+            setIsShift(false);
+        }
+
+        newLayoutName = layout === "default" ? "shift" : "default";
+        setLayout(newLayoutName);
+    };
+
+
+
+    const onKbChange = (input) => {
+        setValue(input)
+    }
+
+    const onKeyPress = button => {
+        if(isShift){
+            handleShift('{shift}');
+            setIsShift(false);
+        }
+        if (button === "SEARCH"){
+            onCancel();
+            return;
+        }
+        if (button === "{shift}" || button === "{lock}"){
+            handleShift(button);
+            return;
+        } 
+    };
+
+    useEffect(() => {
+        keyboard.current.setInput(value);
+    }, [value]);
+
+    return <div className={styles.component} onClick={(e) => {e.preventDefault(); e.stopPropagation();}}>
+        <div className={styles.keyboardInner}>
+            <div className={styles.top}>
+                {/* <div className={styles.keyboardPrev}>
+                    {value}
+                    {!!value && <div className={styles.clear} onClick={() => setValue("")}><BsFillXCircleFill /> Clear</div>}
+                </div>
+                <div className={styles.go} onClick={() => onChange(value)}>
+                    <BsSearch /> Go 
+                </div> */}
+                <div className={styles.cancel} onClick={onCancel}>
+                    <BsFillXCircleFill /> CLOSE
+                </div>
+            </div>
+            <div className={styles.kb}><Keyboard
+                keyboardRef={r => (keyboard.current = r)}
+                onChange={onKbChange}
+                onKeyPress={onKeyPress}
+                layout={{
+                    'default': [
+                        '` 1 2 3 4 5 6 7 8 9 0 - = {bksp}',
+                        '{tab} q w e r t y u i o p [ ] \\',
+                        '{lock} a s d f g h j k l ; \' SEARCH',
+                        '{shift} z x c v b n m , . / {shift}',
+                        '{space}'
+                    ],
+                    'shift': [
+                        '~ ! @ # $ % ^ & * ( ) _ + {bksp}',
+                        '{tab} Q W E R T Y U I O P { } |',
+                        '{lock} A S D F G H J K L : " SEARCH',
+                        '{shift} Z X C V B N M < > ? {shift}',
+                        '{space}'
+                    ]
+                }}
+                layoutName={layout}
+            /></div>
+        </div>
+    </div>
+}
--- renderer/components/VetBridge/VetBridge.jsx
+++ renderer/components/VetBridge/VetBridge.jsx
@@ -1,25 +1,41 @@
-import React, {useEffect, useRef, useState} from 'react';
-import styles from './VetBridge.module.scss';
-import {Modal} from "../Modal/Modal";
-import Dropdown from '../Dropdown/Dropdown';
+import React, { useEffect, useRef, useState } from "react";
+import styles from "./VetBridge.module.scss";
+import { Modal } from "../Modal/Modal";
+// import Dropdown from "../Dropdown/Dropdown";
 import Fuse from "fuse.js";
 
-import 'react-dropdown/style.css';
+import "react-dropdown/style.css";
 import Scroller from "../Scroller/Scroller";
-import {BsSearch} from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
+import Keyboard from "../Keyboard/Keyboard";
 
-export default function VetBridge({allVets}){
+export default function VetBridge({ allVets }) {
   const [currentRecipient, setCurrentRecipient] = useState(null);
   const [displayList, setDisplayList] = useState([]);
-  return <div className={styles.component}>
-    <Filters allVets={allVets} onFilter={filtered => setDisplayList(filtered)} />
-    <List items={displayList} onSetRecipient={recipient => setCurrentRecipient(recipient)} />
-    {!!currentRecipient && <Details item={currentRecipient} onClose={() => setCurrentRecipient(false)}/> }
-  </div>
+  return (
+    <div className={styles.component}>
+      <Filters
+        allVets={allVets}
+        onFilter={(filtered) => setDisplayList(filtered)}
+      />
+      <List
+        items={displayList}
+        onSetRecipient={(recipient) => setCurrentRecipient(recipient)}
+      />
+      {!!currentRecipient && (
+        <Details
+          item={currentRecipient}
+          onClose={() => setCurrentRecipient(false)}
+        />
+      )}
+    </div>
+  );
 }
 
-function Filters({allVets, onFilter}){
-  const [searchText, setSearchText] = useState('');
+function Filters({ allVets, onFilter }) {
+  const [searchText, setSearchText] = useState("");
+  const [activeSearchText, setActiveSearchText] = useState("");
+  const [showKeyboard, setShowKeyboard] = useState(false);
   const fuseInstance = useRef();
   // const [warFilter, setWarFilter] = useState('');
   // const [branchFilter, setBranchFilter] = useState('');
@@ -39,17 +55,15 @@ function Filters({allVets, onFilter}){
   //   }
   // }, [allVets, setFilterSets]);
 
-
   useEffect(() => {
-
-    if(!fuseInstance.current){
+    if (!fuseInstance.current) {
       const options = {
         keys: ["FirstName", "LastName"],
         threshold: -1.2,
         ignoreLocation: true,
         getFn: (obj) => {
-          return obj.FirstName + ' ' + obj.LastName;
-        }
+          return obj.FirstName + " " + obj.LastName;
+        },
       };
       fuseInstance.current = new Fuse(allVets, options);
     }
@@ -58,86 +72,167 @@ function Filters({allVets, onFilter}){
     if (searchText) {
       const result = fuseInstance.current.search(searchText);
       filteredSet = result.map((r) => r.item);
-    } else  {
+    } else {
       filteredSet = allVets;
     }
     onFilter(filteredSet);
+  }, [allVets, activeSearchText]);
 
-  }, [allVets, searchText])
-
-  return <div className={styles.filters}>
-    <div className={styles.searchWrapper}>
-      <BsSearch />
-      <input className={styles.search} placeholder="Search by name..." type="text" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+  return (
+    <div className={styles.filters}>
+      <div className={styles.searchWrapper}>
+        <BsSearch />
+        <input
+          className={styles.search}
+          placeholder="Search by name..."
+          type="text"
+          value={searchText}
+          onClick={() => setShowKeyboard(true)}
+        />
+      </div>
+      <div className={styles.clear} onClick={() => {
+          setActiveSearchText(searchText);
+          setShowKeyboard(false);
+      }}>
+        SEARCH
+      </div>
+      <div className={styles.clear} onClick={() => {
+          setSearchText("");
+          setActiveSearchText("");
+      }}>
+        CLEAR
+      </div>
+      {/*<Dropdown className={styles.wars} items={filterSets.wars} onChange={val => setWarFilter(val)} placeholder="War"/>*/}
+      {/*<Dropdown className={styles.branches} items={filterSets.branches} onChange={val => setBranchFilter(val)} placeholder="Service Branch" />*/}
+      {!!showKeyboard && (
+        <Keyboard
+          onChange={(input) => {
+            setSearchText(input);
+          }}
+          externValue={searchText}
+          onCancel={() => {
+            setShowKeyboard(false)
+            setActiveSearchText(searchText);
+          }}
+        />
+      )}
     </div>
-    <div className={styles.clear} onClick={() => setSearchText('')}>CLEAR</div>
-    {/*<Dropdown className={styles.wars} items={filterSets.wars} onChange={val => setWarFilter(val)} placeholder="War"/>*/}
-    {/*<Dropdown className={styles.branches} items={filterSets.branches} onChange={val => setBranchFilter(val)} placeholder="Service Branch" />*/}
-  </div>
+  );
 }
 
-function List({items, onSetRecipient}){
-  if(!items?.length){
-    return <div className={styles.listContainer}>
+function List({ items, onSetRecipient }) {
+  const [loading, setLoading] = useState(true);
+
+  useState(() => {
+    if (items.length) {
+      setLoading(false);
+    }
+  }, [items]);
+
+  // if (loading) {
+  //   return (
+  //     <div className={styles.listContainer}>
+  //       <div className={styles.listWrapper}>
+  //         <div className={styles.loader}>Loading</div>
+  //       </div>
+  //     </div>
+  //   );
+  // } else 
+  if (!items?.length) {
+    return (
+      <div className={styles.listContainer}>
+        <div className={styles.listWrapper}>
+          <div className={styles.noResults}>
+            No Results. Please try a different name.
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className={styles.listContainer}>
       <div className={styles.listWrapper}>
-        <div className={styles.noResults}>No Results. Please try a different name.</div>
+        <table className={styles.listHead}>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Branch</th>
+              <th>War</th>
+              <th>Dates Active</th>
+              <th></th>
+            </tr>
+          </thead>
+        </table>
+        <Scroller darkBg={true}>
+          <table className={styles.list}>
+            <tbody>
+              {items.map((item) => (
+                <tr
+                  key={item.ID}
+                  className={styles.item}
+                  onClick={() => onSetRecipient(item)}
+                >
+                  <td>{item.FirstName}</td>
+                  <td>{item.LastName}</td>
+                  <td>{item.Branch}</td>
+                  <td>{item.War ? item.War : "-"}</td>
+                  <td>{item.DateAct}</td>
+                  <td className={styles.itemLink}>Details</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Scroller>
       </div>
     </div>
-  }
-  return <div className={styles.listContainer}>
-    <div className={styles.listWrapper}>
-      <Scroller darkBg={true}>
-        <table className={styles.list}>
-          <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Branch</th>
-            <th>War</th>
-            <th>Dates Active</th>
-            <th></th>
-          </tr></thead>
-          <tbody>
-          {items.map(item => <tr key={item.ID} className={styles.item} onClick={() => onSetRecipient(item)}>
-            <td>{item.FirstName}</td>
-            <td>{item.LastName}</td>
-            <td>{item.Branch}</td>
-            <td>{item.War ? item.War : '-'}</td>
-            <td>{item.DateAct}</td>
-            <td className={styles.itemLink}>Details</td>
-          </tr>)}</tbody>
-        </table>
-      </Scroller>
-    </div>
-  </div>
+  );
 }
 
-function Details({item, onClose}){
-  return <Modal onClose={onClose} index={0}><div className={styles.details}>
-    <div className={styles.leftDetails}>
-      <div className={styles.stats}><Scroller>
-        <div className={styles.detailName}>{item.FirstName} {item.MiddleInitial} {item.LastName}</div>
-        <div className={styles.detailHeading}>DETAILS</div>
-        <ul>
-          <li>BORN: {item.BirthDate}</li>
-          <li>BRANCH: {item.Branch}</li>
-          <li>AWARDS: {item.Awards}</li>
-          <li>DEATH: {item.DateDiceased}</li>
-          <li>WAR: {item.War}</li>
-          <li>DATE OF SERVICE: {item.DateAct}</li>
-        </ul>
-        <div className={styles.detailHeading}>INDEX LOCATION ON BRIDGE</div>
-        <ul>
-          <li>Plaque: {item.IndexPlaque}</li>
-          <li>Column: {item.IndexColumn}</li>
-          <li>Row: {item.IndexRow}</li>
-        </ul>
-        <div className={styles.detailHeading}>BIOGRAPHY</div>
-        <p>{item.Biography}</p>
-      </Scroller></div>
-    </div>
-    <div className={styles.rightDetails}>
-      {(item.ImageFile !== 'NOIMAGE') ? <img src={`/vet-images/${item.ImageFile}.jpg`} alt={""} /> :  <img src={`/images/no-photo.png`} alt={""} />}
-    </div>
-  </div></Modal>
+function Details({ item, onClose }) {
+  return (
+    <Modal onClose={onClose} index={0}>
+      <div className={styles.details}>
+        <div className={styles.leftDetails}>
+          <div className={styles.stats}>
+            <Scroller>
+              <div className={styles.detailName}>
+                {item.FirstName} {item.MiddleInitial} {item.LastName}
+              </div>
+              <div className={styles.detailHeading}>DETAILS</div>
+              <ul>
+                <li>BORN: {item.BirthDate}</li>
+                <li>BRANCH: {item.Branch}</li>
+                <li>AWARDS: {item.Awards}</li>
+                <li>DEATH: {item.DateDiceased}</li>
+                <li>WAR: {item.War}</li>
+                <li>DATE OF SERVICE: {item.DateAct}</li>
+              </ul>
+              <div className={styles.detailHeading}>
+                INDEX LOCATION ON BRIDGE
+              </div>
+              <ul>
+                <li>Plaque: {item.IndexPlaque}</li>
+                <li>Column: {item.IndexColumn}</li>
+                <li>Row: {item.IndexRow}</li>
+              </ul>
+              {!!item.Biography && (
+                <>
+                  <div className={styles.detailHeading}>BIOGRAPHY</div>
+                  <p>{item.Biography}</p>
+                </>
+              )}
+            </Scroller>
+          </div>
+        </div>
+        <div className={styles.rightDetails}>
+          {item.ImageFile !== "NOIMAGE" ? (
+            <img src={`/vet-images/${item.ImageFile}.jpg`} alt={""} />
+          ) : (
+            <img src={`/images/no-photo.png`} alt={""} />
+          )}
+        </div>
+      </div>
+    </Modal>
+  );
 }
